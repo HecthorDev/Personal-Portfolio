ae80d239d8617e295320137e42822cefcf287cf3
HecthorDev <hectorgarciajobs19@gmail.com>
2026-01-05 20:50:35 -0600

    Master Prompt Begining

---
+++ 01-foundations/types.js
@@ -0,0 +1,12 @@
+const identificacion = true;
+const edad = 18;
+
+if (edad >= 18) {
+    if (identificacion === true) {
+    console.log("Puedes entrar a la fiesta");
+    } else {
+    console.log("No puedes entrar a la fiesta");
+    }
+} else {
+    console.log("No puedes entrar a la fiesta");
+}
