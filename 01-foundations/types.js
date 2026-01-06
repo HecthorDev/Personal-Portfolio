9bc6f8cd8aacceed8dacda6b12b9714d8ef7fdfa
HecthorDev <hectorgarciajobs19@gmail.com>
2026-01-05 20:51:39 -0600

    Begining

--- 01-foundations/types.js
+++ 01-foundations/types.js
@@ -1,12 +1,37 @@
+//Ejercicio 1: If Nesting
+
 const identificacion = true;
 const edad = 18;
 
 if (edad >= 18) {
-    if (identificacion === true) {
+    if (identificacion) {
     console.log("Puedes entrar a la fiesta");
     } else {
     console.log("No puedes entrar a la fiesta");
     }
 } else {
     console.log("No puedes entrar a la fiesta");
+}
+
+//Ejercicio 2: &&
+
+const identificacion = true;
+const edad = 18;
+
+if (edad >= 18 && identificacion) {
+    console.log("Puedes entrar a la fiesta");
+} else {
+    console.log("No puedes entrar a la fiesta");
+}
+
+// Ejercicio 3: ||
+
+const isStudent = false;
+const isSenior = true;
+
+
+if (isStudent || isSenior) {
+    console.log("No pagas el boleto");
+} else {
+    console.log("Pagas el boleto");
 }
