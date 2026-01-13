90978611f0f4e914850e7823056f08e4666d4c44
HecthorDev <hectorgarciajobs19@gmail.com>
2026-01-12 19:44:28 -0600

    day3

--- 01-foundations/day3.js
+++ 01-foundations/day3.js
@@ -16,3 +16,30 @@ function verificarAcceso(usuario) {
 
 verificarAcceso("Batman");
 verificarAcceso("Robin");
+
+// Ejercicio 3: LA CALCULADORA DE ÁREAS
+
+const ladoDelTerreno = 10; 
+const areaTotal = calcularArea(ladoDelTerreno);
+
+function calcularArea (lado) {
+    return lado * lado;
+}
+
+console.log("El costo es: " + (areaTotal * 5));
+
+// Ejercicio 4: ☠️ BOSS FIGHT DÍA 3: EL CEREBRO DEL SISTEMA
+
+function esSegura(password) {
+    if (password.length >= 8) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const resultado1 = esSegura("patito"); 
+const resultado2 = esSegura("SuperSecret123"); 
+
+console.log(resultado1); 
+console.log(resultado2);
